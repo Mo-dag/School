4c41cc7c3e6e72671872b5d1feca8464987d3c33
Mo-dag <ehsan.mohammad.se@gmail.com>
2026-04-26 00:31:19 +0200

    Add files via upload

---
+++ Real-Time Systems/Lab 2/Part 1/Step 3/App.h
@@ -0,0 +1,23 @@
+#ifndef _APP_H
+#define _APP_H
+
+#include "TinyTimber.h"
+
+typedef struct {
+  Object super;
+  int example;
+  char buffer[20];    // För att lagra inmatade siffror. 
+  int history [20]; // för att lagra de 3 sista siffror. 
+  int index; // för : buffer
+  int cnt;   // för : history
+  int background_loop_range;
+} App;
+
+#define initApp()                                                              \
+  { initObject(), 0,{0},{0},0,0,1000}
+
+void reader(App *, int);
+void receiver(App *, int);
+void startApp(App *, int);
+
+#endif
