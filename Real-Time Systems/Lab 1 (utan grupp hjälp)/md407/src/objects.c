9780b0a3186dff70499ef65f9febf9ad0f7e80b3
Mo-dag <ehsan.mohammad.se@gmail.com>
2026-05-04 17:52:50 +0200

    Delete Real-Time Systems/Lab 1 (utan grupp hjälp) directory

--- Real-Time Systems/Lab 1 (utan grupp hjälp)/md407/src/objects.c
+++
@@ -1,7 +0,0 @@
-#include "App.h"
-#include "canTinyTimber.h"
-#include "sciTinyTimber.h"
-
-App app = initApp();
-Can can0 = initCan(CAN_PORT0, &app, receiver);
-Serial sci0 = initSerial(SCI_PORT0, &app, reader);
