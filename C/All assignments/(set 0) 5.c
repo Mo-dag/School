25751d20005a1afd6f9c502f917dc4a1b2e804f7
Mo-dag <ehsan.mohammad.se@gmail.com>
2026-06-11 01:08:40 +0200

    Itroduction to scanf
    Uppgiftsbeskrivningen finns i set0.pdf

---
+++ C/All assignments/(set 0) 5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declare a variable for storing user input 
+    int a;
+    int b;
+    // Inform the user about what to do
+    printf("Input an integer value: ");
+    scanf("%d", &a);
+    printf("Input an other integer value :");
+    scanf("%d", &b);
+    // Get the input
+    // How this works is explained later. For now, you have to learn this by heart.
+	
+    
+    // Here we just print the input
+	printf("Your input summa is : %d\n", a+b);
+}
