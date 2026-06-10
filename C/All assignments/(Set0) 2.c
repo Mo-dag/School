01d9f2e02394d2bba6d0b26383a4d0bb4f04e5ec
Mo-dag <ehsan.mohammad.se@gmail.com>
2026-06-11 00:07:46 +0200

    Add files via upload

---
+++ C/All assignments/(Set0) 2.c
@@ -0,0 +1,37 @@
+// This example introduces comments and answers 0a-HelloWorld
+
+#include <stdio.h>  // For now you can ignore lines starting with #include
+
+// A comment is text ignored by the compiler, but is added as help to understand
+// the program.
+
+// There are two ways to make comments. This way, using //
+// Everything to the right of // will be ignored (to the end of the line).
+
+/*
+    Here we use the second way. By combining / and * as done here, you can write 
+    comments spanning multiple lines.
+*/
+
+
+int main()   // The program always start where you find this line
+{
+    // Here you have a possible answer to 0a-HelloWorld
+    printf("I am a computer.\nWho are you?\n");
+    
+    /* Another solution, just using one line. Try this version as well! 
+       Comment out the first version, and make this version active. */
+    //printf("I am a computer\nWho are you?\n");
+}
+
+/* 
+   Yes, we can have comments after the end of main. You will see that most of the 
+   code in fact will be found here! 
+*/
+
+// En rad kommentar. 
+/*
+Fler. 
+Rad.
+Kommentar.
+*/
