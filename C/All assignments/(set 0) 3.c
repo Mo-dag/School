d37cf87970eda270996071d3baccf7ad06447077
Mo-dag <ehsan.mohammad.se@gmail.com>
2026-06-11 00:16:46 +0200

    Show uninitialized variable behavior
    Uppgiftsbeskrivningen finns i set0.pdf

---
+++ C/All assignments/(set 0) 3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int c;
+    int a;    // Here we declare (deklarerar) the integer (heltals) variable a
+    a = 7;   // Here we assign (tilldelar) it a value
+    
+    int b = -3;  // Both steps in one go, called initiation (initiering)
+    
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %d\n", b);
+    printf("The value of c is %d\n", c);
+}
