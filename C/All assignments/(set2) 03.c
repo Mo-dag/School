83e15a08e6f0cd4b6e4e1254ccdc9863e1144f50
Mo-dag <ehsan.mohammad.se@gmail.com>
2026-06-17 12:55:28 +0200

    Quadratic equation solver in 03.c
    Implementing a program to solve quadratic equations using the quadratic formula.

---
+++ C/All assignments/(set2) 03.c
@@ -0,0 +1,51 @@
+/*
+ *   Solving a quadratic equation
+ *   https://en.wikipedia.org/wiki/Quadratic_formula
+ *   https://www.mathsisfun.com/quadratic-equation-solver.html
+ *
+ *
+ *   See:
+ *   - math.c
+ */
+#include<stdio.h>
+#include<math.h>
+
+
+int main() {
+
+    printf("Program to solve a quadratic equation: ");
+    printf("ax^2 + bx + c = 0\n");
+    
+    double a;
+    double b;
+    double c;
+    double x1;
+    double x2; 
+    double x;
+    printf("Please input a > ");
+    scanf("%lf", &a);
+    printf("Please input b > ");
+    scanf("%lf", &b);
+    printf("Please input c > ");
+    scanf("%lf", &c);
+
+    double discriminant;
+
+    // TODO
+    discriminant = (b*b - (4*a*c));
+    if( discriminant > 0){
+        printf("There are two distinct real roots ");
+        x1 = (-b+sqrt(discriminant))/(2*a);
+        x2 = (-b-sqrt(discriminant))/(2*a);
+        printf("x1 = %f, x2 = %f\n", x1, x2);
+    } else if( discriminant < 0 ){
+        printf("There are two complex conjugate roots. Can't solve will give nan\n");
+    } else {
+        printf("There are two equal real roots ");
+        x = (-b)/(2*a);
+        printf("x = %lf", x);
+    }
+
+    //printf("x1 = %f, x2 = %f\n", x1, x2);
+    return 0;
+}
