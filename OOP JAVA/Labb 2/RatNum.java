e19e5f40632c1bba5e59d7f01cd2bb1ebcea5699
Mo-dag <ehsan.mohammad.se@gmail.com>
2026-09-14 16:30:32 +0200

    Update RatNum.java

--- OOP JAVA/Labb 2/RatNum.java
+++ OOP JAVA/Labb 2/RatNum.java
@@ -1,10 +1,23 @@
 
-
+//Alla kommentarer som börjar med två stjärnor är JavaDoc Kommentarer.
+// De används för att skriva documentation. Den dokumentara klassens publika gränssnitt.
+/** A class that represents Rational Numbers.
+ * A rational number consists of two integers.
+ * @author Mo Dagher and Akram Abdi
+ * Version 0.1
+ */
 public class RatNum{
 
     private int n;
     private int t;
-
+// Den första meningen i doc berättar vad metoden tillför.
+    /**
+     * Return the greatest common divisor of two integers.
+     * @param n the denominator (nämnaren)
+     * @param t the numerator   (täljaren)
+     * @return the greatest common divisor.
+     * @throws IllegalArgumentException if both n and t are zero.
+     */
     static public int gcd(int n,int t){
 
 
@@ -24,16 +37,31 @@ public class RatNum{
         return GCD;
 
     }
-
+    //En konstruktör har ingen returtyp. Den konstruerar/initierar objektet.
+    /**
+     * Constructs a rational number representing zero.
+     */
     public RatNum(){
         this.n=1;
         this.t=0;
     }
+
+    /**
+     * Constructs a rational number representing the value of t
+     * @param t the numerator
+     */
     public RatNum(int t){
         this.t = t;
         this.n = 1;
     }
 
+    /**
+     * Constructs a rational number in reduced form with numerator t and denominator n.
+     * @param t the numerator
+     * @param n the denominator
+     * @throws NumberFormatException if the denominator is zero.
+     */
+    //↓↓↓↓↓↓↓↓↓↓↓ En konstruktör som fyller en obj. Måste ; new RatNum(värde,värde) →→→→→ RatNum obj = new RatNum(värde, värde);
     public RatNum(int t,int n){
         if(n == 0){throw new NumberFormatException("Denominator = 0");}
         int delare = gcd(n,t);
@@ -44,45 +72,60 @@ public class RatNum{
         this.n = n/delare;
         this.t = t/delare;
     }
+
+    /**
+     * Constructs a rational number as a copy of another RatNum obj.
+     * @param x is the RatNum objekt to copy.
+     */
     public RatNum(RatNum x){
         this.n = x.n;
         this.t = x.t;
+
+        //new RatNum(a)
+        //↓
+        //skapar nytt objekt
+        //↓
+        //RatNum(RatNum x) körs
+        //↓
+        //kopierar x.n och x.t
+
     }
+
+    /**
+     * Returns the numerator.
+     * @return the numerator value.
+     */
     public int getNumerator(){
         return this.t;
     }
+
+    /**
+     * Return the denominator
+     * @return the denominator value.
+     */
+
     public int getDenominator(){
         return this.n;
     }
 
+    /**
+     * Returns this rational number as a string.
+     * @return the numerator and denominator as text.
+     */
     public String toString(){
         return t + "/"+n;
     }
 
-  // Klass metod (static) kan inte använda instansvariabler direkt(instansvariabler : private int n; private int t;)
-  // Reason : tillhörighet.
-  //DVS klass metoden : parse(), tillhör klassen.  
-  // Instansvariabler tillhör objekt. 
-    // med andra ord : är den static metod, kan man inte använda : this.instansvariable;
-  
-    public static RatNum parse(String s){
-        //Korrigerat: Så en klassmetod kan inte läsa eller ändra instansvariabler direkt. 
-        //Klassmetoden tillhör klassen, medan instansvariablerna tillhör objekt.
-        // Vårt problem här är att vi ska skapa en metod som tar en String,
-        // kontrollerar den och sedan returnerar ett nytt rationellt tal med det godkända värdet.
-        // Det enda rationella talet här är alltså ett objekt av klassen RatNum, eller? 
-        //Eftersom ett rationellt tal representeras av två int-värden.
-
-
-        //För att kontorllera om värdet är giltig, vi skulle filtrera värdena och spara de i int array. 
-        // För att värdena är sträng typ och finns det / med de. 
-        // Vi kommer att använda split. split() är en funk av klassen String.
-        // Den delar Java sträng 
-
-        String[] newS = s.split("/");
-        // Villkoret är : om n = 0 »» fel // om n inte finns »» n = 1;
+    /**
+     * Take a rational number from a string and returns it in reduced form as RatNum object.
+     * @param s the string to parse.
+     * @return RatNum object representing the rational number.
+     * @throws NumberFormatException if the string is invalid or the denominator is Zero.
+     */
+    public static RatNum parse(String s) {
         int n;
         int t;
+        String [] newS = s.split("/");
         if(newS.length == 1){n = 1;}
         else{
             n = Integer.parseInt(newS[1]);
@@ -93,17 +136,27 @@ public class RatNum{
         // Vi filtrerade värdena och omvandlade de till int.
         // För att spara värdena i instansvariabler anropar vi
         // RatNum som vi skapade innan, den är inte static och kan använda this.t , this.n
-        // Men för att den är inte static, den tillhör Obj. Då : vi behöver skapa ett nytt obj. new.
+        // Men för att den är inte static, den tillhör Obj. Då : vi behöver skapa ett nytt obj.
         return new RatNum(t,n);
+        // Parse skapar en separat RatNum obj
+        // problemet att : RatNum obj : utan namn !!! Vår funk returnerar vad ?
+        // Den returnerar REFERENS Referensen till det objektet.
+        // Så parse() kan inte stå helt själv. Den returnerar en referens till ett RatNum-objekt. /
+        // Vi måste ta emot den referensen om vi vill behålla de behandlade värdena.
     }
-    // Nu : De vill att vi återanvänder logiken i parse() i stället för att skriva samma kontrollkod igen.
-     public RatNum(String s){
-        //RatNum obj = new RatNum(parse(s)); DETTA ÄR FEEEEEEEEEEEL !!!!
+
+    /**
+     * Constructs a rational number from a string.
+     * @param s the string to parse.
+     * @throws NumberFormatException if the string is invalid.
+     */
+    public RatNum(String s){
+        //RatNum obj = new RatNum(parse(s)); Detta är fel !
         //Problemet är att parse() returnerar en referens, inte själva objektet.
         //Vi behöver en bro som tar värdena n och t »»
         // från objektet som referensen pekar på och kopierar dem till det nya objektet.
         this(parse(s));
-        // This : letar efter en konstruktor som tar emot referenser som parse producerar.
+        // This : letar efter en konstruktör som tar emot referenser som parse producerar.
 
         //parse("3/4")
         //↓
@@ -121,23 +174,25 @@ public class RatNum{
         //        ↓
         //   objekt B [t=3, n=4]
     }
-        // equals måste skrivas med exakt rätt parameter för att fungerar.
+
+    /**
+     * Checks if the rational number is equals to other rational number of the same type and return true or false.
+     * @param obj the object to compare with
+     * @return true if the objects represents the same rational number, otherwise false.
+     */
+    // equals måste skrivas med exakt rätt parameter för att fungerar.
     //Så vi vill ändra den och skapa vår egen equals()
     // för att kunna jämföra två olika objekt utifrån deras innehåll
     // inte bara två referenser som pekar på samma objekt
     @Override    // Ersätter javas gamla equals.
     public boolean equals(Object obj){
-        // Är obj inte ett RatNum ? "Vi använder instansof : Java-keyword ✅ "
+        // Är obj inte ett RatNum ? "Vi använder instanceof : Java-keyword ✅ "
         if(!(obj instanceof RatNum)){
             return false;
         }
-        //if(this.getClass() != obj.getClass) ← ← ← Detta är lärarens sätt att skriva villkoret. 
-        //if(!(obj instansof RatNum)          ← ← ← Vår sätt. Både gör samma sak.
-
-        
         // obj redan pekar på ett RatNum obj.
         // obj »» typ Object
-        // vi skulle skapa en ny referensvariable av typen RatNum för att få värde i den korrekta typen
+        // vi skulle skapa en ny referensvariabel av typen RatNum för att få värde i den korrekta typen
         // Vi castar obj till RatNum så att r kan använda RatNum:s fält och metoder.
 
         RatNum r = (RatNum) obj;
@@ -160,6 +215,13 @@ public class RatNum{
     // Kommer vi att använda kors-multi
     // a/b < c/d ?
     // a * d < c * b ?
+
+    /**
+     * Checks whether this rational number is less than the given rational number.
+     * @param r the rational number to compare with.
+     * @return true if this rational number is less than r, otherwise false.
+     */
+
     public boolean lessThan(RatNum r){
 
         if ((this.t*r.n)<(r.t*this.n)){
@@ -169,6 +231,13 @@ public class RatNum{
 
         // return this.t * r.n < r.t * this.n;
     }
+
+    /**
+     * Adds the given rational number to this rational number.
+     * @param r the given rational numebr to add.
+     * @return a referens to a new RatNum object representing the sum.
+     */
+
     // För att addera två olika RatTal, skulle vi ha en gemensam nämnare.
     // a * d + c * b / b*d
     public RatNum add(RatNum r){
@@ -177,12 +246,24 @@ public class RatNum{
         sum.n = this.n * r.n;
         return new RatNum(sum.t,sum.n);
     }
+
+    /** Subtract the given rational number from this rational number.
+     *
+     * @param r the given rational number to subtract.
+     * @return a referens to a new RatNum obj representing the sum.
+     */
     public RatNum sub(RatNum r){
         RatNum sum = new RatNum();
         sum.t = (this.t*r.n) - (this.n*r.t);
         sum.n = this.n * r.n;
         return new RatNum(sum.t,sum.n);
     }
+
+    /**
+     * Multiply a given RatNum with this rational number.
+     * @param r the given rational number to multiply.
+     * @return a referens to a new RatNum obj representing the sum.
+     */
     public RatNum mul(RatNum r){
         RatNum sum = new RatNum();
         sum.n = this.n*r.n;
@@ -195,6 +276,12 @@ public class RatNum{
     //↓
     //(a*d) / (b*c)
 
+
+    /**
+     * Divide a given rational number with this rational number
+     * @param r the given rational number to divide.
+     * @return a referens to a new RatNum obj representing the sum.
+     */
     public RatNum div(RatNum r){
         RatNum sum = new RatNum();
         sum.t = this.t * r.n;
@@ -202,8 +289,14 @@ public class RatNum{
 
         return new RatNum(sum.t,sum.n);
     }
+
+    /**
+     * Returns an integer number in form of string from this rational number.
+     * @return the integer value of this rational number as a string.
+     */
     public String toIntString(){
         int tal = this.t/this.n;
         return Integer.toString(tal);
     }
+
 }
